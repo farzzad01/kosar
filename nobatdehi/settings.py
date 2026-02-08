daeabb0b20af315c9ca635ead9bdb694712ff887
farzad <farzad.ghlm8@gmail.com>
2026-02-09 00:42:50 +0330

    Configure Django for Vercel deployment

--- nobatdehi/settings.py
+++ nobatdehi/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-gow$dt_n7skv)ez8-3=pp@rjubv)t#18mdkyuk_sp=y-cg)=tt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -117,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
---
+++ vercel_wsgi.py
@@ -0,0 +1,3 @@
+from nobatdehi.wsgi import application
+
+app = application
